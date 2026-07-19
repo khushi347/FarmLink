008f9929169fa6897a2455d8a6ad807917a630fb
Khushi <khushi240305@gmail.com>
2026-07-19 19:50:08 +0530

    feat: add speech processing pipeline using Sarvam AI

--- backend/src/controllers/handlewebHook.js
+++ backend/src/controllers/handlewebHook.js
@@ -1,24 +1,32 @@
 const downloadAudio = require("../utils/downloadAudio");
+const { speechToText } = require("../services/speechService");
 
-const handlewebHook=async(req,res)=>{
-    try{
+const handlewebHook = async (req, res) => {
+    try {
         console.log(req.body);
-        if(req.body.MediaContentType0?.startsWith("audio")){
-            const mediaUrl=req.body.MediaUrl0;
 
-            const filePath=await downloadAudio(
+        if (req.body.MediaContentType0?.startsWith("audio")) {
+
+            const mediaUrl = req.body.MediaUrl0;
+
+            const filePath = await downloadAudio(
                 mediaUrl,
                 `${Date.now()}.ogg`
             );
 
-            console.log("Voice saved at:",filePath)
+            console.log("Voice saved at:", filePath);
+
+            const transcript = await speechToText(filePath);
+
+            console.log("Transcript:", transcript);
         }
 
         res.sendStatus(200);
-    }catch(error){
+
+    } catch (error) {
         console.error(error);
         res.sendStatus(500);
     }
-}
+};
 
-module.exports=handlewebHook;
+module.exports = handlewebHook;
